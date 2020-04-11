2e7b430b63b71fc4c4f6fdb1d6ff4bffa0915191
Jarrod Finn <jarrod.p.finn@gmail.com>
2020-04-11 13:28:45 -0400

    updated server.js

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+// const PORT = 3000;
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(logger("dev"));
 
